b5bb2371952651b5218d94cdb66601f89ecbe6bf
Stevany Angel <stevanyangel_2327240033@mhs.mdp.ac.id>
2025-11-08 18:41:44 +0700

    Component

---
+++ basic_react/src/BasicComponent.tsx
@@ -0,0 +1,40 @@
+import { Button } from "react-bootstrap";
+import MyButton from "./components/ui/Button";
+import StateCounter from "./components/ui/StateCounter";
+
+// parent component
+function BasicComponent() {
+  return <>
+  {/*child component*/}
+  <MyButton>
+    Test
+  </MyButton>
+
+  <MyButton>
+    Ini buttonku, mana buttonmu
+  </MyButton>
+
+  <MyButton>
+    Simpan
+  </MyButton>
+
+  <StateCounter/>
+
+  <Button variant="warning">Test Component Lagi</Button>
+  </>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default BasicComponent;
--- basic_react/src/main.tsx
+++ basic_react/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import App from './App.tsx'
+import BasicComponent from './BasicComponent.tsx'
+
+//createRoot(document.getElementById('root')!).render(
+//  <StrictMode>
+//    <App />
+//  </StrictMode>,
+//)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BasicComponent />
   </StrictMode>,
 )
